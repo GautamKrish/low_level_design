da9d20c711930a46e74171f19c966c60d7f02a6e
gautam.krishnan <gautam.krishnan@servicenow.com>
2022-06-14 22:29:03 +0530

    Builder v2

--- Creational_Design_Patterns/src/Builder/Client.java
+++ Creational_Design_Patterns/src/Builder/Client.java
@@ -1,2 +1,8 @@
-package Builder;public class Client {
+package Builder;
+
+public class Client {
+	public static void main(String[] args) {
+		Student_v1 object_1 = new Student_v1("Gautam Krishnan", "33378755", "gomzi@yahoo.com", "7067543906", 25);
+		// not at all readable and scalable
+	}
 }
--- Creational_Design_Patterns/src/Builder/Student_v1.java
+++ Creational_Design_Patterns/src/Builder/Student_v1.java
@@ -1,2 +1,21 @@
-package Builder;public class version_1 {
+package Builder;
+/*
+1. A class with lot of attributes;
+2. Immutable class; (cannot change state of an instance once it is created)
+*/
+
+public class Student_v1 {
+	String name;
+	String id;
+	String email;
+	String contact;
+	int age;
+
+	public Student_v1(String name, String id, String email, String contact, int age){
+		this.name = name;
+		this.id = id;
+		this.email = email;
+		this.contact = contact;
+		this.age = age;
+	}
 }
